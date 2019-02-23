a64d42dc90caf1c20016e4227c6a95a038dec077
essential-gemfile-5901 <kyle.coberly@gmail.com>
2019-02-23 23:21:58 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,23 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves dwarves
+  dwarves.each.with_index(1) do |name, index|
+    puts "#{index}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet planeteer_calls
+  planeteer_calls.map { |name| "#{name.capitalize}!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls planeteer_calls
+  planeteer_calls.any? { |call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese food
   cheese_types = ["cheddar", "gouda", "camembert"]
+  matches = food.select { |item| cheese_types.include? item }
+  if matches.empty?
+    return nil
+  else
+    return matches.first
+  end
 end
